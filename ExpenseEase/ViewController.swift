445234a50dc574549e8ee3ebf4190ab99ac4b44c
iris19951229 <shaonian1229@gmail.com>
2024-10-23 15:22:02 -0700

    Initial Commit

---
+++ ExpenseEase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseEase
+//
+//  Created by Guangmei Xiang on 10/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
